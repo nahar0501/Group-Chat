3f4ea0e83885e0db43b8e7202626d1fe303f9d5d
Fahad <fadiktk000@gmail.com>
2022-07-20 22:28:13 -0700

    Fahad

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:chat_app/views/chat_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return  MaterialApp(
+      theme: ThemeData(
+        appBarTheme: AppBarTheme(
+          color: Colors.white,
+          elevation: 0,
+        )
+      ),
+      home: ChatScreen(),
+      );
+  }
+}
---
+++ lib/views/chat_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class ChatScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+         Container(
+           height: 50,
+         )
+        ],
+      ),
+    );
+  }
+}
